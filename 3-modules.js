517641379a06581d8d44cc80a7cd327f7da4f560
RHODA <njerirhoda31@gmail.com>
2023-09-16 07:37:47 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//CommonJS, every file is module (by default)
+// Modules - Encapsuated Code (only share minimum)
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-syntax')
+require('./7-mind-grenade')
+
+//sayHi('Susan')
+//sayHi(names.john)
+//sayHi(names.peter)
